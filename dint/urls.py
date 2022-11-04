95aa53639bdacaaac2fee60fc3a61c161d0d607e
maxxpa <info@dint.com>
2022-11-04 11:36:23 -0400

    Revert "Revert "resolve images file solution s3 bucket""
    This reverts commit ae3292bd663c4efbf96c015777677ee2aa0c9632.

--- dint/urls.py
+++ dint/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_swagger.views import get_swagger_view
+from django.conf.urls.static import static
 
 schema_view = get_swagger_view(title='Dint-API')
 
@@ -25,3 +27,5 @@ urlpatterns = [
     path('chat/', include('chat.urls')),
     path('docs/', schema_view),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
