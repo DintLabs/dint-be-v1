37aa4a10ae61e81ab023bab028308e78be9f6d05
Deepak Batra <deepakb@squadmindsinc.com>
2022-11-09 18:24:45 +0530

    resolve issues in post pagination by user id page and sort bookmark list

--- api/services/posts/postService.py
+++ api/services/posts/postService.py
@@ -58,16 +58,16 @@ class PostsService (PostsBaseService):
 
         post_type = request.data.get('post_type')
         if post_type is None or post_type == 'all':
-            post_obj = Posts.objects.all()
+            # post_obj = Posts.objects.all()
+            post_obj = Posts.objects.filter() 
+            print(post_obj)
         else:
             post_obj = Posts.objects.filter(type = post_type)
 
         follower_list = UserFollowers.objects.filter(follower = request.user.id).values_list('user')
-
         follower_post_obj = post_obj.filter(user__in = follower_list)
         own_post_obj = post_obj.filter(user = request.user.id)
         final_obj = follower_post_obj | own_post_obj
-
 
         custom_pagination = CustomPagination()
         search_keys = ['content__icontains', 'id__contains']
@@ -210,7 +210,8 @@ class PostsService (PostsBaseService):
         custom_pagination = CustomPagination ()
         search_keys = ['content__icontains', 'id__contains']
         search_type = 'or'
-        roles_response = custom_pagination.custom_pagination(request, Posts, search_keys, search_type, GetPostsSerializer, post_obj)
+        context = {"logged_in_user":request.user.id}
+        roles_response = custom_pagination.custom_pagination(request, Posts, search_keys, search_type, GetPostsSerializer, post_obj , context)
         return {"data": roles_response['response_object'],
                 "recordsTotal": roles_response['total_records'],
                 "recordsFiltered": roles_response['total_records'],
