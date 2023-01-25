1da3df21e077adcc9519f4f8b32bcad6a167b3c1
Max <info@dint.com>
2023-01-25 05:43:10 -0500

    No need

--- dint/settings.py
+++ dint/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','api.dint.com', 'bedev.dint.com','paydev.dint.com', '32h19eg817.dint.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'bedev.dint.com','32h19eg817.dint.com']
 
 
 
