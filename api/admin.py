e6998754cf8bbd51f111b16253e9f77b36768bb3
dhruvpy <dhrvnarola@gmail.com>
2023-02-20 11:29:20 +0530

    django admin ui fix

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models.PayoutsModel import Payouts
+from .models import *
 
 # Register your models here.
-admin.site.register(Payouts)
+admin.site.register(User)
