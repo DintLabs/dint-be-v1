1eb5d37b72f1c259f80ee5d9b2e07a474439a24b
Ved Anshu <vedanshullr@gmail.com>
2023-02-21 15:28:57 +0530

    Get and send message notifications

--- api/urls.py
+++ api/urls.py
@@ -147,6 +147,7 @@ urlpatterns = [
     path('chat/search_user/', SearchUserView.as_view(), name="create-posts"),
     path('chat/create-notification/', ListCreateUpdateDeleteNotificationView.as_view(), name="create-notification"),
     path('chat/get-notification-by-user-in-chunks/<int:pk>/', ListNotificationsChunksView.as_view(), name="create-notification"),
+    path('chat/get-unseen-message/', GetUnseenChatByUser.as_view(), name="unseen-chat"),
     #Page
     path('page/list/', ListCreateUpdateDeletePageView.as_view(), name="get-all-page"),
     path('page/pagination/list/', GetPagePaginationView.as_view(), name="get-all-page"),
