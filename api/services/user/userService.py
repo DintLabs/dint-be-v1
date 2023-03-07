a06f4ef495ee0f34fc0087bf24aaa627776f7c87
Max <info@dint.com>
2023-03-07 09:58:20 -0500

    Update userService.py

--- api/services/user/userService.py
+++ api/services/user/userService.py
@@ -228,7 +228,7 @@ class UserService(UserBaseService):
                 chain_id = settings.CHAIN_ID
                 register = contract.functions.register(new_user, referral_address).buildTransaction({  
                     'from': user_address,
-                    'chainId': 80001,   
+                    'chainId': CHAIN_ID,   
                     'gasPrice': web3.toWei('30', 'gwei'),  
                     'nonce': nonce,  
                 })  
@@ -340,7 +340,7 @@ class UserService(UserBaseService):
 
         sendReward = contract.functions.reward(receiver_add, tip_dint).buildTransaction({
             'from': user_address,
-            'chainId': 80001,  
+            'chainId': CHAIN_ID,  
             'gasPrice': web3.toWei('30', 'gwei'),  
             'nonce': new_nonce,     
         })
