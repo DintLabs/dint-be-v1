0708413688fcaa814ae7556afce2eb85b46ef18c
Max <info@dint.com>
2023-03-24 19:18:05 -0400

    Create 0114_add_media_column_to_messages_history.py

---
+++ api/migrations/0114_add_media_column_to_messages_history.py
@@ -0,0 +1,15 @@
+from django.db import migrations, models
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0112_auto_20230307_1053'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='historicalmessages',
+            name='media',
+            field=models.CharField(max_length=255, null=True),
+        ),
+    ]
