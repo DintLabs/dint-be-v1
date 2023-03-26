80efaf9f1a7dab7394477da6ebba4fb9732a78b9
Max <info@dint.com>
2023-03-25 22:11:01 -0400

    Update userService.py

--- api/services/user/userService.py
+++ api/services/user/userService.py
@@ -61,7 +61,7 @@ from itertools import chain
 from api.utils.token import account_activation_token
 import redis
 from dint.settings import *
-from web3.exceptions import TransactionNotFound
+
 class UserService(UserBaseService):
     """
     Allow any user (authenticated or not) to access this url 
@@ -261,7 +261,7 @@ class UserService(UserBaseService):
         try:
             response = requests.post(url, headers = headers, data = payload)
             data = response.json()
-            Hash = data['hash']
+            Hash = data['Hash']
             node_url = settings.NODE_URL
             web3 = Web3(Web3.HTTPProvider(node_url))
             dintReceipt = web3.eth.wait_for_transaction_receipt(Hash)  
@@ -297,8 +297,9 @@ class UserService(UserBaseService):
                 
             else:
                 return ({"data": data, "code": status.HTTP_400_BAD_REQUEST, "message": "Transaction Failed"})
-        except TransactionNotFound:
-               return ({"data": [], "code": status.HTTP_400_BAD_REQUEST, "message": "Transaction not found. Please try again later."})
+        except:
+             return ({"data": [], "code": status.HTTP_400_BAD_REQUEST, "message": "Oops Sending! Something went wrong."})
+      
         
     def send_reward_by_token(self, request, format=None):
         receiver = User.objects.get(id = request.user.id)
