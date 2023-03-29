323c554bf62020b6f79447287cc8fd8452d84cb3
Max <info@dint.com>
2023-03-28 22:54:27 -0400

    Update userService.py

--- api/services/user/userService.py
+++ api/services/user/userService.py
@@ -303,7 +303,7 @@ class UserService(UserBaseService):
                       node_url = settings.NODE_URL
                       web3 = Web3(Web3.HTTPProvider(node_url))
                       dintReceipt = web3.eth.wait_for_transaction_receipt(Hash, timeout=120)
-                      if dintReceipt:
+                      if (dintReceipt):
                            return JsonResponse({"data": data, "message": "Token sent successfully"}, status=201)     
                else:
                   return JsonResponse({"data": [], "message": "Transaction Failed"}, status=400)
